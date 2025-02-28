ab5e2061734e8764178f720bd23949ab32b888ad
MaxeneP <89520410+MaxeneP@users.noreply.github.com>
2025-02-28 23:42:44 +0800

    Delete FurEver directory

--- FurEver/utils/furever_password.js
+++
@@ -1,9 +0,0 @@
-function togglePassword(inputId) {
-    let passwordInput = document.getElementById(inputId);
-    
-    if (passwordInput.type === "password") {
-        passwordInput.type = "text"; // Change to normal text
-    } else {
-        passwordInput.type = "password"; // Change back to hidden
-    }
-}
