3bb52808dbc3211a4d7ae408fefb1b9b4f9db790
ace-chan-423 <abchan@mymail.mapua.edu.ph>
2025-07-19 06:15:11 +0800

    Made editlist responsive

--- FurEver/utils/editlist.js
+++ FurEver/utils/editlist.js
@@ -24,6 +24,17 @@ document.addEventListener("DOMContentLoaded", async function () {
             locationObject.showProvinces('#provinces');
             window.addEventListener('click', eventDelegation);
             provinceDrop.addEventListener('change', provinceChange);
+            window.addEventListener('resize', fontChange);
+            fontChange();
+        }
+
+        function fontChange() {  
+            parent = document.querySelector('.photo-slot')
+            uploadIcons = document.querySelectorAll('.photo-slot>i');
+            for (let icon of uploadIcons) {
+                fontSize = parent.offsetHeight - 50;
+                icon.style.fontSize = `${fontSize}px`;
+            }
         }
 
         function eventDelegation(e) {
